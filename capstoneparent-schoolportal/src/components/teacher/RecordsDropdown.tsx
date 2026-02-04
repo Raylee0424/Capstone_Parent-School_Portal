2650e92024b7e564ba522cfa0878f6a3ef9ce06d
StarMysteries <95sl%c0>
2026-02-04 20:07:46 +0800

    Fixes

---
+++ capstoneparent-schoolportal/src/components/teacher/RecordsDropdown.tsx
@@ -0,0 +1,24 @@
+export const RecordsDropdown = () => {
+  return (
+    <div className="absolute mt-2 w-48 bg-(--navbar-bg) border border-gray-200 rounded-md shadow-lg z-10">
+      <a
+        href="/classlists"
+        className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+      >
+        Class Lists
+      </a>
+      <a
+        href="/manageparentverification"
+        className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+      >
+        Manage Parent Verification
+      </a>
+      <a
+        href="/managestudents"
+        className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+      >
+        Manage Students
+      </a>
+    </div>
+  );
+};
